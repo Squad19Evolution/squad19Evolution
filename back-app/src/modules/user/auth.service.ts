fc61563cd20b359ee36c2f398fa1c08551a65992
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-11-04 19:42:04 -0300

    feat: add login mtd to auth service

--- back-app/src/modules/user/auth.service.ts
+++ back-app/src/modules/user/auth.service.ts
@@ -1,14 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { randomBytes, scrypt as _scrypt } from 'crypto';
 import { promisify } from 'util';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UserLoginDto } from './dto/user-login.dto';
+import { UserRepository } from './user.repository';
 import { UserService } from './user.service';
 
 const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private userRepo: UserRepository,
+  ) {}
 
   async signup(user: CreateUserDto) {
     const salt = randomBytes(8).toString('hex');
@@ -16,5 +21,15 @@ export class AuthService {
     const result = salt + '.' + hash.toString('hex');
     user.password = result;
     return this.userService.createUser(user);
+  }
+
+  async login({ email, password }: UserLoginDto) {
+    const user = await this.userRepo.findByEmail(email);
+    if (!user) throw new BadRequestException('User or password incorrect');
+    const [salt, storedHash] = user.password.split('.');
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    if (storedHash !== hash.toString('hex'))
+      throw new BadRequestException('User or password incorrect');
+    return user;
   }
 }
