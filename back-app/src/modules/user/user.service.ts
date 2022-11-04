942c669a42f8a5cc65f3076e7da9a6bb53703641
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-11-04 18:42:40 -0300

    chore: set up user repo

--- back-app/src/modules/user/user.service.ts
+++ back-app/src/modules/user/user.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UserRepository } from './user.repository';
 
 @Injectable()
 export class UserService {
+  constructor(private userRepo: UserRepository) {}
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
