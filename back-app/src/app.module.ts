d67054efa841406ba465f9e8c9bd9946b81c4255
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-11-04 17:07:49 -0300

    chore: set up db

--- back-app/src/app.module.ts
+++ back-app/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './modules/user/entities/user.entity';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'orange_db',
+      password: 'password',
+      username: 'root',
+      port: 5432,
+      database: 'db',
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
