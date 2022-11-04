aae4379233d16820fab2991c3adc9d9cf8953307
JeanVictorL <jeanvictor.lopes@gmail.com>
2022-11-04 20:30:24 -0300

    feat:router start

--- front-app/src/App.jsx
+++ front-app/src/App.jsx
@@ -1,3 +1,5 @@
+import { Link, Route, Routes } from "react-router-dom"
+import Landing from "./pages/Landing"
 import Login from "./pages/Login"
 
 
@@ -6,7 +8,16 @@ function App() {
 
   return (
     <div className="App">
-      <Login />
+      <nav>
+        <ul>
+          <li><Link to="/" >Landing</Link ></li>
+          <li><Link to="/login" >login</Link ></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   )
 }
