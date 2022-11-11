a4b8332b1cfd02103274c28562dc45de97b9616b
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-11-11 14:24:41 -0300

    refactor: add custom serializer to content controller

--- back-app/src/modules/content/content.controller.ts
+++ back-app/src/modules/content/content.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { ContentService } from './content.service';
+import { ContentDto } from './dto/content-dto';
 import { CreateContentDto } from './dto/create-content.dto';
 
 @Controller('content')
@@ -7,16 +9,13 @@ export class ContentController {
   constructor(private readonly contentService: ContentService) {}
 
   @Post()
+  @Serialize(ContentDto)
   create(@Body() createContentDto: CreateContentDto) {
     return this.contentService.create(createContentDto);
   }
 
-  @Get()
-  findAll() {
-    return this.contentService.findAll();
-  }
-
   @Get(':id')
+  @Serialize(ContentDto)
   findOne(@Param('id') id: string) {
     return this.contentService.findById(+id);
   }
