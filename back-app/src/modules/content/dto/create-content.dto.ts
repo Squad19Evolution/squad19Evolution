71e334583c2f870109f26ff52e65e32ff1519469
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-11-11 00:16:45 -0300

    feat: ad create-content & create-path dtos

--- back-app/src/modules/content/dto/create-content.dto.ts
+++ back-app/src/modules/content/dto/create-content.dto.ts
@@ -1,1 +1,15 @@
-export class CreateContentDto {}
+import { IsNumber, IsString } from 'class-validator';
+
+export class CreateContentDto {
+  @IsNumber()
+  path_id: number;
+
+  @IsString()
+  name: string;
+
+  @IsString()
+  url: string;
+
+  @IsString()
+  duration: string;
+}
