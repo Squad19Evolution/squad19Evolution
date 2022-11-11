5299de849890e41088653a311f719714cfaf1bc9
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-11-11 14:23:27 -0300

    featt: add create & findById mtds in content service

--- back-app/src/modules/content/content.service.ts
+++ back-app/src/modules/content/content.service.ts
@@ -1,17 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PathService } from '../path/path.service';
+import { ContentRepository } from './content.repository';
 import { CreateContentDto } from './dto/create-content.dto';
 
 @Injectable()
 export class ContentService {
-  create(createContentDto: CreateContentDto) {
-    return 'This action adds a new content';
+  constructor(
+    private contentRepo: ContentRepository,
+    private pathService: PathService,
+  ) {}
+
+  async create(createContentDto: CreateContentDto) {
+    const path = await this.pathService.findById(createContentDto.path_id);
+    return this.contentRepo.create(createContentDto, path);
   }
 
-  findAll() {
-    return `This action returns all content`;
-  }
-
-  findById(id: number) {
-    return `This action returns a #${id} content`;
+  async findById(id: number) {
+    const content = await this.contentRepo.findById(id);
+    if (!content) throw new NotFoundException('Content not found');
+    return content;
   }
 }
